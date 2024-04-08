1bc567b4f4dd6915b183513198f7b28cd5f45e1a
Michał Przyłucki <michal.przyl@gmail.com>
2024-04-08 21:12:26 +0200

    First try with text rendering

--- main.c
+++ main.c
@@ -1,8 +1,40 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
-
+#include <SDL2/SDL_ttf.h>
+#include <dirent.h> 
 // #include <SDL/include/SDL.h>
 // #include "SDL/include/SDL"
+
+void read_directory_files(){
+    DIR *d;
+    struct dirent *dir;
+    d = opendir(".");
+    if (d) {
+    while ((dir = readdir(d)) != NULL) {
+        printf("%s\n", dir->d_name);
+    }
+    closedir(d);
+    }
+}
+
+void get_text_and_rect(SDL_Renderer *renderer, int x, int y, char *text,
+        TTF_Font *font, SDL_Texture **texture, SDL_Rect *rect) {
+    int text_width;
+    int text_height;
+    SDL_Surface *surface;
+    SDL_Color textColor = {255, 255, 255, 0};
+
+    surface = TTF_RenderText_Solid(font, text, textColor);
+    *texture = SDL_CreateTextureFromSurface(renderer, surface);
+    text_width = surface->w;
+    text_height = surface->h;
+    SDL_FreeSurface(surface);
+    rect->x = x;
+    rect->y = y;
+    rect->w = text_width;
+    rect->h = text_height;
+}
+
 
 int main(int argc, char *argv[]){
     int WIDTH = 400;
@@ -17,13 +49,31 @@ int main(int argc, char *argv[]){
     // draw color
     SDL_SetRenderDrawColor(renderer, 235, 168, 52, 255);
 
+// print text in window
+
+    //this opens a font style and sets a size
+    TTF_Init();
+    // TTF_Font* Sans = TTF_OpenFont("CascadiaMono.ttf", 24);
+    TTF_Font *font = TTF_OpenFont("CascadiaMono.ttf", 24);
+
+// this is the color in rgb format,
+// maxing out all would give you the color white,
+// and it will be your text's color
+SDL_Color White = {255, 255, 255};
+
+// as TTF_RenderText_Solid could only be used on
+// SDL_Surface then you have to create the surface first
+
+// now you can convert it into a texture
+
+
     SDL_RenderClear(renderer);
     SDL_RenderPresent(renderer);
     if (window == NULL){
         puts("cant create window");
         return 1;
     }
-
+    read_directory_files();
     SDL_Event windowEvent;
     while (1)
     {
@@ -34,7 +84,7 @@ int main(int argc, char *argv[]){
         }
     }
 
-    
+
     SDL_DestroyWindow( window );
     SDL_Quit( );
 }
